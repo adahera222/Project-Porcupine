30804c53585ec0ac7b143ce50df508d3bfa6604f
Martin "quill18" Glaude <quill18@towerdive.com>
2012-01-27 16:26:51 -0500

    Improved vision, now respects line of sight.

--- Assets/Scripts/Creatures/VisionRadius.cs
+++ Assets/Scripts/Creatures/VisionRadius.cs
@@ -7,7 +7,24 @@ public class VisionRadius : MonoBehaviour {
 		//Debug.Log( "VisionRadius::OnTriggerEnter" );
 		//Debug.Log( other );
 		if( other.tag == "Creature" ) {
-			Debug.Log( this + " can see " + other );
+			
+			CharacterController myCC = this.transform.parent.GetComponent<CharacterController>();
+			CharacterController otherCC = other.collider.GetComponent<CharacterController>();
+			
+			//Debug.Log( myCC + " is trying to see " + otherCC );
+			
+			RaycastHit hit;
+			if (Physics.Raycast (
+					transform.position+myCC.center, 
+					otherCC.transform.position-transform.position, 
+					out hit)
+				) {
+				CharacterController hitCC = hit.collider.GetComponent<CharacterController>();
+				//Debug.Log(hit.collider);
+				if(otherCC==hitCC) {
+					Debug.Log( this + " can see " + otherCC );
+				}
+			}
 		}
 	}
 }
